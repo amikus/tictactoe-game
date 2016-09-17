3e770457ecf8a33458a7eea2b3bf7181fa5d6f04
Andrew Klem <amklem@gmail.com>
2016-09-17 15:13:10 -0400

    Improve comments and remove timer

--- Main.cpp
+++ Main.cpp
@@ -45,41 +45,37 @@ void myInit(void)
 	glLoadIdentity();
 	gluOrtho2D(0.0, (GLdouble)screenWidth, 0.0, (GLdouble)screenHeight);
 
-	A = screenWidth / 4.0;					// set values used for scaling and shifting
+	// set values used for scaling and shifting
+	A = screenWidth / 4.0;					
 	B = 0.0;
 	C = D = screenHeight / 2.0;
 
 
 }
 
-//<<<<<<<<<<<<<<<<<<<<<<<< myTimer >>>>>>>>>>>>>>>>>
-void myTimer(int iUnused)
-{
-	// update animations and redraw
-	// glutPostRedisplay();
-}
 
 //<<<<<<<<<<<<<<<<<<<<<<<< myIdle >>>>>>>>>>>>>>>>>
 void myIdle()
 {
-	// update animations and redraw
+	// redraw the screen / update animations
 	glutPostRedisplay();
 }
 
 //<<<<<<<<<<<<<<<<<<<<<<<< myDisplay >>>>>>>>>>>>>>>>>
 void myDisplay(void)
 {
-	glClear(GL_COLOR_BUFFER_BIT);			// clear the screen 
+	// clear screen to background color
+	glClear(GL_COLOR_BUFFER_BIT);
 
+	// draw gameboard
 	gameboard->drawGameBoard();
 
+	// draw pieces
 	xPiece1->draw(GL_RENDER);
 	oPiece1->draw(GL_RENDER);
 
-	glutSwapBuffers();						// swap buffers
-
-											// call timer to update animations and screen again
-	glutTimerFunc(30, myTimer, 0);
+	// swap buffers
+	glutSwapBuffers();
 
 }
 
@@ -88,20 +84,25 @@ void myDisplay(void)
 int main(int argc, char** argv)
 {
 
+	// Game Pieces
 	xPiece1 = new XPiece(550, 150, XPIECE);
 	oPiece1 = new OPiece(550, 75, OPIECE);
 
-	glutInit(&argc, argv);          // initialize the toolkit
+	// Basic glut setup
+	glutInit(&argc, argv);							// initialize the toolkit
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);	// set display mode
 	glutInitWindowSize(screenWidth, screenHeight);	// set window size
 	glutInitWindowPosition(100, 150);				// set window position on screen
-	glutCreateWindow("Tic Tac Toe");		// open the screen window
+	glutCreateWindow("Tic Tac Toe");				// open the screen window
 
-									// register callback functions
+	// register callback functions
+
+
 	glutDisplayFunc(myDisplay);     // register redraw function
 
 
 	myInit();						// additional initializations
+
 
 	glutIdleFunc(myIdle);
 
